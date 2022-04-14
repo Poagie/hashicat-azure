7a38d14f9258d5065310964d480ff2e83b4cf6b0
Andrew Poage <abpactor@hotmail.com>
2022-04-14 17:22:41 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/PoageCorp/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "Networks"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
